f4f460c414d00142a4090b39dd45419a35010645
Eric Dugre <EricD@kentico.com>
2020-09-24 14:16:02 -0400

    Update for Xperience 13

---
+++ Old_App_Code/CMSModules/Xperience.Zapier/ZapierWorkflowAction.cs
@@ -0,0 +1,18 @@
+﻿using CMS.DocumentEngine;
+using CMS.EventLog;
+
+namespace Xperience.Zapier
+{
+    public class ZapierWorkflowAction : DocumentWorkflowAction
+    {
+        public override void Execute()
+        {
+            var url = GetResolvedParameter("WebhookURL", string.Empty);
+            if(url != string.Empty) ZapierHelper.SendPostToWebhook(url, Node.Site, Node);
+            else
+            {
+                EventLogProvider.LogEvent("W", nameof(ZapierWorkflowAction), "EXECUTE", $"Workflow '{Workflow.WorkflowDisplayName}' step 'Send page to Zapier' couldn't be processed because it is missing the webhook URL.");
+            }
+        }
+    }
+}
